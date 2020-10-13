1eb7fda3a1362b7a098d0be7eb915d14fbf550da
Piter <pomaranski.piotr@gmail.com>
2020-10-13 12:33:00 +0200

    news/blog app added, main view, css, bootstrap, base template created

--- Yahting/Yahting/settings.py
+++ Yahting/Yahting/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
+    'blog.apps.BlogConfig',
     'school.apps.SchoolConfig',
     'django.contrib.admin',
     'django.contrib.auth',
